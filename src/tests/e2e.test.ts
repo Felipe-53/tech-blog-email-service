4ce5ab7066c587902912b791183f157ae6f50214
Felipe53 <felipeandresb97@gmail.com>
2023-01-20 12:59:09 -0300

    aditional e2e test

--- src/tests/e2e.test.ts
+++ src/tests/e2e.test.ts
@@ -1,14 +1,18 @@
 import { FastifyInstance } from "fastify";
-import { beforeEach, expect } from "vitest";
+import { afterEach, beforeEach, expect } from "vitest";
 import { test } from "vitest";
 import { buildServer } from "../api/server";
+import { clearDb } from "../recipients/repos/InMemoryRecipientRepo";
 
 let server: FastifyInstance;
 
 beforeEach(() => {
-  server = buildServer({
-    // logger: true,
-  });
+  server = buildServer();
+});
+
+afterEach(() => {
+  clearDb();
+  server.close();
 });
 
 test("Should be able to create a recipient", async () => {
@@ -24,4 +28,49 @@ test("Should be able to create a recipient", async () => {
   });
 
   expect(response.statusCode).toBe(201);
+});
+
+test("Should NOT be able to create a recipient with invalid email", async () => {
+  const response = await server.inject({
+    path: "/recipient",
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    payload: {
+      email: "invalid.email",
+    },
+  });
+
+  expect(response.statusCode).toBe(400);
+});
+
+test.only("Should NOT be able to create a recipient the was already created", async () => {
+  const email = "felipe@email.com";
+
+  const firstResponse = await server.inject({
+    path: "/recipient",
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    payload: {
+      email,
+    },
+  });
+
+  expect(firstResponse.statusCode).toBe(201);
+
+  const secondResponse = await server.inject({
+    path: "/recipient",
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    payload: {
+      email,
+    },
+  });
+
+  expect(secondResponse.statusCode).toBe(409);
 });
