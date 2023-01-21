94484928ed944a1d77234454f3d93d2406d00643
Felipe53 <felipeandresb97@gmail.com>
2023-01-21 11:18:45 -0300

    better identifier class interface

--- src/shared/Identifier.ts
+++ src/shared/Identifier.ts
@@ -1,19 +1,22 @@
 import { randomUUID } from "crypto";
+import { z } from "zod";
+import { ValidationError } from "./errors";
 
+const uuidSchema = z.string().uuid();
 export class Identifier {
-  private id: string;
+  private constructor(private id: string) {}
 
-  private constructor(id?: string) {
-    if (id) {
-      // TODO: validate UUID
-      this.id = id;
-      return;
-    }
-    this.id = randomUUID();
+  public static create() {
+    const uuid = randomUUID();
+    return new Identifier(uuid);
   }
 
-  public static create(id?: string) {
-    return new Identifier(id);
+  public static instantiate(id: string) {
+    const result = uuidSchema.safeParse(id);
+    if (result.success) {
+      return new Identifier(result.data);
+    }
+    throw new ValidationError(result.error.message);
   }
 
   getValue() {
