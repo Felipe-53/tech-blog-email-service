7843163f5b7a4f78a15416ca0795fdd48b7037ad
Felipe53 <felipeandresb97@gmail.com>
2023-01-22 15:39:36 -0300

    adds recipient routes

--- src/recipients/infra/recipientRoutes.ts
+++ src/recipients/infra/recipientRoutes.ts
@@ -1,4 +1,7 @@
-import { FastifyPluginAsync } from "fastify";
+import { FastifyPluginAsync, FastifyRequest } from "fastify";
+import { z } from "zod";
+import { zodToJsonSchema } from "zod-to-json-schema";
+import { confirmRecipientController } from "../use-cases/ConfirmRecipient";
 import { createRecipientController } from "../use-cases/CreateRecipient/index";
 
 export const recipientRoutes: FastifyPluginAsync = async (app) => {
@@ -7,5 +10,39 @@ export const recipientRoutes: FastifyPluginAsync = async (app) => {
     method: "POST",
     handler: createRecipientController.execute.bind(createRecipientController),
     schema: createRecipientController.schema,
+  });
+
+  app.route({
+    url: "/recipient/:id/status",
+    method: "PATCH",
+    handler: confirmRecipientController.execute.bind(
+      confirmRecipientController
+    ),
+    schema: confirmRecipientController.schema,
+  });
+
+  const confirmRecipientSchema = z.object({
+    id: z.string().uuid(),
+  });
+
+  type ConfirmRecipientSchema = z.infer<typeof confirmRecipientSchema>;
+
+  app.route({
+    url: "/confirm-recipient",
+    method: "GET",
+    handler: (
+      req: FastifyRequest<{ Querystring: ConfirmRecipientSchema }>,
+      reply
+    ) => {
+      const recipientId = req.query.id;
+
+      reply.status(301);
+      reply.headers({
+        location: `https://felipebarbosa.dev/email-subscription?id=${recipientId}`,
+      });
+    },
+    schema: {
+      querystring: zodToJsonSchema(confirmRecipientSchema),
+    },
   });
 };
