59f9ffdfc57207faccca89a9e35d6e2a644a9233
Felipe53 <felipeandresb97@gmail.com>
2023-01-22 15:40:13 -0300

    adds confirm recipient e2e test

--- src/tests/e2e.test.ts
+++ src/tests/e2e.test.ts
@@ -2,6 +2,8 @@ import { FastifyInstance } from "fastify";
 import { afterEach, beforeEach, expect } from "vitest";
 import { test } from "vitest";
 import { buildServer } from "../api/server";
+import { Recipient } from "../recipients/Recipient";
+import { RecipientDTO } from "../recipients/RecipientDTO";
 import { clearDb } from "../recipients/repos/InMemoryRecipientRepo";
 
 let server: FastifyInstance;
@@ -28,6 +30,12 @@ test("Should be able to create a recipient", async () => {
   });
 
   expect(response.statusCode).toBe(201);
+
+  const { recipient } = response.json<{ recipient: RecipientDTO }>();
+
+  expect(recipient.email).toBe("felipe@email.com");
+  expect(new Date(recipient.subscribedAt)).toBeInstanceOf(Date);
+  expect(recipient.confirmedAt).toBeNull();
 });
 
 test("Should NOT be able to create a recipient with invalid email", async () => {
@@ -73,4 +81,36 @@ test("Should NOT be able to create a recipient the was already created", async (
   });
 
   expect(secondResponse.statusCode).toBe(409);
+});
+
+test("Should be able to confirm a recipient", async () => {
+  const email = "felipe@email.com";
+
+  const createResponse = await server.inject({
+    path: "/recipient",
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    payload: {
+      email,
+    },
+  });
+
+  const { recipient: createdRecipient } = createResponse.json<{
+    recipient: RecipientDTO;
+  }>();
+  expect(createdRecipient.confirmedAt).toBeNull();
+
+  const confirmResponse = await server.inject({
+    path: `/recipient/${createdRecipient.id}/status`,
+    method: "PATCH",
+  });
+
+  expect(confirmResponse.statusCode).toBe(200);
+
+  const { recipient: confirmedRecipient } = confirmResponse.json<{
+    recipient: RecipientDTO;
+  }>();
+  expect(new Date(confirmedRecipient.confirmedAt!)).toBeInstanceOf(Date);
 });
