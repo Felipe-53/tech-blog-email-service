49fbf4985b641a68147ce5a64a360d1b9d44d648
Felipe53 <felipeandresb97@gmail.com>
2023-01-23 15:39:28 -0300

    updates lambda handler function

--- src/queue-worker/handler.ts
+++ src/queue-worker/handler.ts
@@ -1,7 +1,29 @@
 import { SQSHandler } from "aws-lambda";
+import { z } from "zod";
+import { sendNewBlogPostEmail } from "./use-cases/sendEmail";
 
-const handler: SQSHandler = async (event, context) => {
-  event.Records.forEach((record) => {
-    record.body;
-  });
+const postDataSchema = z.object({
+  title: z.string(),
+  excerpt: z.string(),
+  link: z.string(),
+});
+
+export const handler: SQSHandler = async (event, context) => {
+  for (const record of event.Records) {
+    const body = JSON.parse(record.body);
+
+    const result = postDataSchema.safeParse(body);
+
+    if (!result.success) {
+      throw result.error;
+    }
+
+    const postData = result.data;
+
+    await sendNewBlogPostEmail({
+      postTitle: postData.title,
+      postExcerpt: postData.excerpt,
+      postLink: postData.link,
+    });
+  }
 };
