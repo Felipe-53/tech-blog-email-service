d277ae1fbc238893be3e90742a3d0d0f9485ed8f
Felipe53 <felipeandresb97@gmail.com>
2023-01-27 14:21:48 -0300

    adds send email feature upon recipient creation

--- src/recipients/use-cases/CreateRecipient/CreateRecipientController.ts
+++ src/recipients/use-cases/CreateRecipient/CreateRecipientController.ts
@@ -6,6 +6,8 @@ import { InconsistentDataError } from "../../../shared/errors";
 import { Recipient } from "../../Recipient";
 import { zodToJsonSchema } from "zod-to-json-schema";
 import { RecipientMap } from "../../RecipientMap";
+import { env } from "../../../env";
+import { SGMailService } from "../../../email-service/SGMailService";
 
 const createRepicientBody = z.object({
   email: z.string().email(),
@@ -32,6 +34,22 @@ export class CreateRecipientController extends BaseController {
         return this.conflict(reply, error);
       }
       throw error;
+    }
+
+    if (env.node_env === "production") {
+      const sgClient = new SGMailService(env.send_grid_api_key);
+      const response = await sgClient.send({
+        from: "felipeasbarbosa.dev@gmail.com",
+        to: recipient.email.getValue(),
+        templateId: "d-dab1027475ce4df18cd0d27f50cd1869",
+        dynamicTemplateData: {
+          id: recipient.id.getValue(),
+        },
+      });
+      if (!response.accepted) {
+        reply.log.error("Unable to send email");
+        reply.log.error(response.info);
+      }
     }
 
     return this.created(reply, {
