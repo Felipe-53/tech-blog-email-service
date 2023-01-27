082748170cb1d8d3a00f1a35223a2fb4360b1eb3
Felipe53 <felipeandresb97@gmail.com>
2023-01-27 14:20:30 -0300

    adds node_env to env

--- src/env.ts
+++ src/env.ts
@@ -4,10 +4,12 @@ import { config } from "dotenv";
 config();
 
 const env = {
+  node_env: process.env.NODE_ENV as string,
   send_grid_api_key: process.env.SEND_GRID_API_KEY as string,
   database_url: process.env.DATABASE_URL as string,
 };
 
+assertNonEmptyString(env.node_env);
 assertNonEmptyString(env.send_grid_api_key);
 assertNonEmptyString(env.database_url);
 
