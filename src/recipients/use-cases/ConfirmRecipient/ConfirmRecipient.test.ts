45b84d1716beea98935ef298478b235846d681d6
Felipe53 <felipeandresb97@gmail.com>
2023-02-02 19:52:07 -0300

    update tests

--- src/recipients/use-cases/ConfirmRecipient/ConfirmRecipient.test.ts
+++ src/recipients/use-cases/ConfirmRecipient/ConfirmRecipient.test.ts
@@ -1,10 +1,17 @@
-import { expect, test } from "vitest";
+import { expect, test, afterEach } from "vitest";
 import { ValidationError } from "../../../shared/errors";
-import { InMemoryRecipientRepo } from "../../repos/InMemoryRecipientRepo";
+import {
+  clearDb,
+  InMemoryRecipientRepo,
+} from "../../repos/InMemoryRecipientRepo";
 import { CreateRecipient } from "../CreateRecipient/CreateRecipient";
 import { ConfirmRecipient } from "./ConfirmRecipient";
 
 const inMemoryRecipientRepo = new InMemoryRecipientRepo();
+
+afterEach(() => {
+  clearDb();
+});
 
 test("Should be able to confirm a recipient", async () => {
   const createRecipient = new CreateRecipient(inMemoryRecipientRepo);
@@ -28,4 +35,25 @@ test("Should not be able to confirm a recipient that doesn't exist", async () =>
       id: "non-existing",
     })
   ).rejects.toBeInstanceOf(ValidationError);
+});
+
+test("Should be able to confirm a recipient twice without errors", async () => {
+  const createRecipient = new CreateRecipient(inMemoryRecipientRepo);
+  const confirmRecipient = new ConfirmRecipient(inMemoryRecipientRepo);
+
+  const createdRecipient = await createRecipient.execute({
+    email: "felipe@email.com",
+  });
+
+  const confirmedRecipient = await confirmRecipient.execute({
+    id: createdRecipient.id.getValue(),
+  });
+
+  expect(confirmedRecipient.confirmedAt).not.toBeNull();
+
+  const confirmedTwice = await confirmRecipient.execute({
+    id: confirmedRecipient.id.getValue(),
+  });
+
+  expect(confirmedTwice.confirmedAt).not.toBeNull();
 });
